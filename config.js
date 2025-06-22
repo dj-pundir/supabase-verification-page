12fde803bfd9132187681f1fd17afe4cd366fc73
Digvijay Pundir (DJ) <61174028+dj-pundir@users.noreply.github.com>
2025-06-22 17:52:09 +0530

    Create config.js

---
+++ config.js
@@ -0,0 +1,4 @@
+window.SUPABASE_CONFIG = {
+  url: "https://pndatyazqkfjrhqjuhfz.supabase.co",
+  anonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBuZGF0eWF6cWtmanJocWp1aGZ6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTA0Mzc4ODksImV4cCI6MjA2NjAxMzg4OX0.BVJYFGX73Cz8Ji4Nif56mCRA36Ek-OlmapKVvKjW7Fo"
+};
